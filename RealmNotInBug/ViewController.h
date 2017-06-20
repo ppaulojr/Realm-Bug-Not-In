b49572960d30472698e3e02e4bafa6a36167bcf5
Pedro Paulo Oliveira Junior <ppaulojr@gmail.com>
2017-06-20 11:23:52 -0300

    Initial Commit

---
+++ RealmNotInBug/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RealmNotInBug
+//
+//  Created by Pedro Paulo Oliveira Junior on 6/20/17.
+//  Copyright © 2017 ppaulojr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ RealmNotInBug/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  RealmNotInBug
+//
+//  Created by Pedro Paulo Oliveira Junior on 6/20/17.
+//  Copyright © 2017 ppaulojr. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
